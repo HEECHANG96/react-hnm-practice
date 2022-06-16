aed90841b93bef3b33f4b4983a0461cd7c7bd1ec
Heechang Jeong <wjdgmlckd96@daum.net>
2022-06-16 18:37:23 +0900

    first commit

---
+++ src/component/ProductCard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProductCard = () => {
+  return (
+    <div>
+        <img src=""/>
+        <div>dd</div>
+        <div>dd</div>
+        <div>dd</div>
+        <div>dd</div>
+    </div>
+  )
+}
+
+export default ProductCard
